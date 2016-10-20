ba00c48e88eee812f386fce0f3ab91a7c8a13772
supriyapremkumar <spremkum@ucsc.edu>
2016-10-20 16:36:02 -0700

    First Commit

---
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
@@ -0,0 +1,99 @@
+package com.codepath.android.booksearch.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.adapters.BookAdapter;
+import com.codepath.android.booksearch.models.Book;
+import com.codepath.android.booksearch.net.BookClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class BookListActivity extends AppCompatActivity {
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+    private BookClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<>();
+        // initialize the adapter
+        bookAdapter = new BookAdapter(this, aBooks);
+        // attach the adapter to the ListView
+        lvBooks.setAdapter(bookAdapter);
+        // Fetch the data remotely
+        fetchBooks("Oscar Wilde");
+    }
+
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchBooks(String query) {
+        client = new BookClient();
+        client.getBooks(query, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray docs;
+                    if(response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+                        // Load model objects into the adapter
+                        for (Book book : books) {
+                            bookAdapter.add(book); // add book through the adapter
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
